7df8b8a93ab168ebf1f8ba2acb3ec79cc0f5aaa6
jacobhouts <jacob.houts@gmail.com>
2021-05-13 20:35:43 -0400

    groceryhw

---
+++ groceryhw/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+import Grocery from './grocery';
+function App() {
+  return (
+    <div className="">
+      <h1> Grocery List</h1>
+       <Grocery /> 
+    </div>
+  );
+}
+export default App;
